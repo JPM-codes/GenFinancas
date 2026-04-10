e6513fd3f59244559a37a91f05003101a3ebba8f
JPM-codes <mellojoaopedro20@gmail.com>
2026-04-10 20:49:58 -0300

    Atualiza páginas de movimentação para incluir funcionalidades de edição e inserção de novas movimentações

--- pages/movimentacao/edit.php
+++ pages/movimentacao/edit.php
@@ -1,46 +1,123 @@
 <?php
-    $titulo_pagina = "Alterando o Tipo de Despesa";
+$titulo_pagina = "Inserindo Tipo de Despesa";
 
-    require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/../../config/database.php';
 
-    include '../../templates/header.php';
+include '../../templates/header.php';
 
-    include '../../templates/navbar.php';
+include '../../templates/navbar.php';
 
 
-    // Alterando o tipo de despesa
-    $id = $_GET['id_tipo_despesa'];
-    $stmt = $pdo->prepare('SELECT id_tipo_despesa, descricao FROM tipo_despesa WHERE id_tipo_despesa=?');
+$tipo_despesa = $pdo->query
+('SELECT id_tipo_despesa, descricao FROM tipo_despesa
+     WHERE ind_excluido = 0 
+     ORDER BY descricao')
+    ->fetchAll();
 
-    $stmt->execute([$id]);
-    $tipo_despesa = $stmt->fetch();
+$tipo_pagamento = $pdo->query
+('SELECT id_forma_pagamento, descricao FROM forma_pagamento
+     WHERE ind_excluido = 0 
+     ORDER BY descricao')
+    ->fetchAll();
 
-    if(!$tipo_despesa) {
-        die('Tipo de despesa não encontrada!');
-    }
+$id = $_GET['id_movimentacao'];
+$stmt = $pdo->prepare('
+        SELECT id_movimentacao, id_tipo_despesa, id_forma_pagamento, tipo_movimentacao, data, valor, observacao
+        FROM movimentacao
+        WHERE id_movimentacao = ?
+    ');
+$stmt->execute([$id]);
+$movimentacao = $stmt->fetch();
 
-    if (isset($_POST['edit'])) {
-        $stmt = $pdo->prepare('UPDATE tipo_despesa SET descricao=? WHERE id_tipo_despesa=?');
-        $stmt->execute([$_POST['descricao'], $id]);
-        header('Location: index.php');
-    }
+if (!$movimentacao) {
+    echo "Movimentação não encontrada.";
+    exit;
+}
+
+// Inserindo um novo tipo de despesa
+if (isset($_POST['add'])) {
+
+    
+
+    header("Location: index.php");
+}
 ?>
-    <div class="container-fluid">
-      <h3>Inserindo um novo tipo de despesa</h3>
-      <div class="card mb-4">
-            <div class="card-body">
-                <form method="POST" class="row g-3">
-                    <div class="col-md-10">
-                        <input type="text" name="descricao" value="<?= $tipo_despesa['descricao'] ?>" placeholder="Descrição do tipo de despesa" class="form-control" required>
+<div class="container py-4">
+    <h3 class="mb-4">Inserindo uma nova movimentação</h3>
+    <div class="card shadow-sm">
+        <div class="card-body p-4">
+            <form method="POST">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="form-group mb-3">
+                            <label class="form-label fw-bold">Tipo de Movimentação</label>
+                            <select class="form-select" name="tipo_movimentacao" id="tipo_movimentacao">
+                                <option value="e" <?php if ($movimentacao['tipo_movimentacao'] === 'e')
+                                    echo 'selected'; ?>>Entrada</option>
+                                <option value="s" <?php if ($movimentacao['tipo_movimentacao'] === 's')
+                                    echo 'selected'; ?>>Saída</option>
+                            </select>
+                        </div>
                     </div>
-                    <div class="col-md-2">
-                        <button type="submit" name="edit" class="btn btn-success">Salvar</button>
+                    <div class="col-4">
+                        <div class="form-group mb-3">
+                            <label class="form-label fw-bold">Tipo de Despesa</label>
+                            <select class="form-select" name="tipo_despesa" id="tipo_despesa">
+                                <option value="">Selecione...</option>
+                                <?php foreach ($tipo_despesa as $t): ?>
+                                    <option value="<?= $t['id_tipo_despesa'] ?>" 
+                                    <?php if ($movimentacao['id_tipo_despesa'] == $t['id_tipo_despesa']) echo 'selected'; ?>>
+                                    <?= $t['descricao'] ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
                     </div>
-                </form>
-            </div>
-      </div>
+                    <div class="col-4">
+                        <div class="form-group mb-3">
+                            <label class="form-label fw-bold">Forma de Pagamento</label>
+                            <select class="form-select" name="forma_pagamento" id="forma_pagamento">
+                                <option value="">Selecione...</option>
+                                <?php foreach ($tipo_pagamento as $t): ?>
+                                    <option value="<?= $t['id_forma_pagamento'] ?>"
+                                    <?php if ($movimentacao['id_forma_pagamento'] == $t['id_forma_pagamento']) echo 'selected'; ?>>
+                                    <?= $t['descricao'] ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-2">
+                        <div class="form-group mb-3">
+                            <label class="form-label fw-bold">Data</label>
+                            <input type="date" class="form-control" name="data" id="data_movimentacao" value="<?= $movimentacao['data'] ?>">
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="form-group mb-3">
+                            <label class="form-label fw-bold">Valor</label>
+                            <input type="text" class="form-control" name="valor" id="valor_movimentacao"
+                                placeholder="0,00" value="<?= $movimentacao['valor'] ?>">
+                        </div>
+                    </div>
+                    <div class="col-8">
+                        <div class="form-group mb-4">
+                            <label class="form-label fw-bold">Observação de lançamento</label>
+                            <input type="text" class="form-control" name="observacao" id="observacao_movimentacao" value="<?= $movimentacao['observacao'] ?>">
+                        </div>
+                    </div>
+                </div>
+
+
+                <button type="submit" name='edit' class="btn btn-success px-5">
+                    Editar
+                </button>
+            </form>
+        </div>
     </div>
+</div>
 
 <?php
-    include '../../templates/footer.php';
+include '../../templates/footer.php';
 ?>
--- pages/movimentacao/new.php
+++ pages/movimentacao/new.php
@@ -7,11 +7,31 @@ include '../../templates/header.php';
 
 include '../../templates/navbar.php';
 
+$tipo_despesa = $pdo->query
+('SELECT id_tipo_despesa, descricao FROM tipo_despesa
+     WHERE ind_excluido = 0 
+     ORDER BY descricao')
+    ->fetchAll();
+
+$tipo_pagamento = $pdo->query
+('SELECT id_forma_pagamento, descricao FROM forma_pagamento
+     WHERE ind_excluido = 0 
+     ORDER BY descricao')
+    ->fetchAll();
 
 // Inserindo um novo tipo de despesa
 if (isset($_POST['add'])) {
-    $stmt = $pdo->prepare("INSERT INTO tipo_despesa (descricao) VALUES (?)");
-    $stmt->execute([$_POST['descricao']]);
+    $stmt = $pdo->prepare("INSERT INTO movimentacao (id_tipo_despesa, id_forma_pagamento, tipo_movimentacao, data,valor,observacao) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->execute(
+        [
+            $_POST['tipo_despesa'],
+            $_POST['forma_pagamento'],
+            $_POST['tipo_movimentacao'],
+            $_POST['data'],
+            $_POST['valor'],
+            $_POST['observacao']
+        ]
+    );
     header("Location: index.php");
 }
 ?>
@@ -19,12 +39,14 @@ if (isset($_POST['add'])) {
     <h3 class="mb-4">Inserindo uma nova movimentação</h3>
     <div class="card shadow-sm">
         <div class="card-body p-4">
-            <form>
+            <form method="POST">
                 <div class="row">
                     <div class="col-4">
                         <div class="form-group mb-3">
                             <label class="form-label fw-bold">Tipo de Movimentação</label>
                             <select class="form-select" name="tipo_movimentacao" id="tipo_movimentacao">
+                                <option value="entrada">Entrada</option>
+                                <option value="saida">Saída</option>
                             </select>
                         </div>
                     </div>
@@ -32,6 +54,10 @@ if (isset($_POST['add'])) {
                         <div class="form-group mb-3">
                             <label class="form-label fw-bold">Tipo de Despesa</label>
                             <select class="form-select" name="tipo_despesa" id="tipo_despesa">
+                                <option value="">Selecione...</option>
+                                <?php foreach ($tipo_despesa as $t): ?>
+                                    <option value="<?= $t['id_tipo_despesa'] ?>"><?= $t['descricao'] ?></option>
+                                <?php endforeach; ?>
                             </select>
                         </div>
                     </div>
@@ -39,6 +65,10 @@ if (isset($_POST['add'])) {
                         <div class="form-group mb-3">
                             <label class="form-label fw-bold">Forma de Pagamento</label>
                             <select class="form-select" name="forma_pagamento" id="forma_pagamento">
+                                <option value="">Selecione...</option>
+                                <?php foreach ($tipo_pagamento as $t): ?>
+                                    <option value="<?= $t['id_forma_pagamento'] ?>"><?= $t['descricao'] ?></option>
+                                <?php endforeach; ?>
                             </select>
                         </div>
                     </div>
@@ -48,26 +78,26 @@ if (isset($_POST['add'])) {
                     <div class="col-2">
                         <div class="form-group mb-3">
                             <label class="form-label fw-bold">Data</label>
-                            <input type="date" class="form-control" name="data_movimentacao" id="data_movimentacao">
+                            <input type="date" class="form-control" name="data" id="data_movimentacao">
                         </div>
                     </div>
                     <div class="col-2">
                         <div class="form-group mb-3">
                             <label class="form-label fw-bold">Valor</label>
-                            <input type="text" class="form-control" name="valor_movimentacao" id="valor_movimentacao" placeholder="0,00">
+                            <input type="text" class="form-control" name="valor" id="valor_movimentacao"
+                                placeholder="0,00">
                         </div>
                     </div>
                     <div class="col-8">
                         <div class="form-group mb-4">
                             <label class="form-label fw-bold">Observação de lançamento</label>
-                            <select class="form-select" name="observacao_movimentacao" id="observacao_movimentacao">
-                            </select>
+                            <input type="text" class="form-control" name="observacao" id="observacao_movimentacao">
                         </div>
                     </div>
                 </div>
 
 
-                <button type="submit" class="btn btn-success px-5">
+                <button type="submit" name='add' class="btn btn-success px-5">
                     Salvar
                 </button>
             </form>
