39110380d1ab0e03f9aaf51ba01e727f86276723
JPM-codes <mellojoaopedro20@gmail.com>
2026-04-10 20:51:33 -0300

    Adiciona estrutura e dados para a tabela 'movimentacao' no banco de dados

--- BD_financeiro.sql
+++ BD_financeiro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 28/03/2026 às 00:13
+-- Tempo de geração: 11/04/2026 às 01:51
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -45,6 +45,30 @@ INSERT INTO `forma_pagamento` (`id_forma_pagamento`, `descricao`, `ind_excluido`
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `movimentacao`
+--
+
+CREATE TABLE `movimentacao` (
+  `id_movimentacao` int(11) NOT NULL,
+  `id_tipo_despesa` int(11) NOT NULL,
+  `id_forma_pagamento` int(11) NOT NULL,
+  `tipo_movimentacao` varchar(1) DEFAULT NULL,
+  `valor` float DEFAULT NULL,
+  `data` date DEFAULT NULL,
+  `observacao` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `movimentacao`
+--
+
+INSERT INTO `movimentacao` (`id_movimentacao`, `id_tipo_despesa`, `id_forma_pagamento`, `tipo_movimentacao`, `valor`, `data`, `observacao`) VALUES
+(1, 3, 3, 'e', 150, '2026-04-10', 'teste'),
+(3, 3, 1, 'e', 379, '2026-04-18', 'Testee');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `tipo_despesa`
 --
 
@@ -72,6 +96,14 @@ ALTER TABLE `forma_pagamento`
   ADD PRIMARY KEY (`id_forma_pagamento`);
 
 --
+-- Índices de tabela `movimentacao`
+--
+ALTER TABLE `movimentacao`
+  ADD PRIMARY KEY (`id_movimentacao`),
+  ADD KEY `id_tipo_despesa` (`id_tipo_despesa`),
+  ADD KEY `id_forma_pagamento` (`id_forma_pagamento`);
+
+--
 -- Índices de tabela `tipo_despesa`
 --
 ALTER TABLE `tipo_despesa`
@@ -88,10 +120,27 @@ ALTER TABLE `forma_pagamento`
   MODIFY `id_forma_pagamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT de tabela `movimentacao`
+--
+ALTER TABLE `movimentacao`
+  MODIFY `id_movimentacao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT de tabela `tipo_despesa`
 --
 ALTER TABLE `tipo_despesa`
   MODIFY `ID_TIPO_DESPESA` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `movimentacao`
+--
+ALTER TABLE `movimentacao`
+  ADD CONSTRAINT `movimentacao_ibfk_1` FOREIGN KEY (`id_tipo_despesa`) REFERENCES `tipo_despesa` (`ID_TIPO_DESPESA`),
+  ADD CONSTRAINT `movimentacao_ibfk_2` FOREIGN KEY (`id_forma_pagamento`) REFERENCES `forma_pagamento` (`id_forma_pagamento`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
